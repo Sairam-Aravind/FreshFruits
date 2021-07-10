072eda80799c29561560582fa9645cca0a6870da
Sairam Aravind M <sairam.mr@zohocorp.com>
2021-07-10 16:15:10 +0530

    Made the code modular, Added Unit tests for methods, increased the margin for radio button container input

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+let pageIndexMismatchError = "Index mismatch error";
+let answersmissing = "Answers not available in local storage";
+
+
+
+
+
 // decides which method to load based on the last known value of the index variable in local storage
 const main = () => {
   let index = localStorage.getItem("index");
@@ -10,21 +17,56 @@ const main = () => {
     loadFirst();
   }
 };
+
+
+
+
+
 // loads the first page of the feedback form. Button is "not-allowed" until the fetch call is successful
 const loadFirst = async () => {
-  document.getElementById("survey-start").style.display = "flex";
-  document.getElementById("survey-end").style.display = "none";
-  document.getElementById("survey-container").style.display = "none";
+  controlDisplay("flex", "none", "none");
   let proceedButton = document.getElementById("proceed-btn");
-  proceedButton.style.backgroundColor = "grey";
-  proceedButton.style.cursor = "not-allowed";
-  let questions = await fetch(
+  disableProceedButton(proceedButton);
+  let questions = await fetchQuestions();
+  localStorage.setItem("questions", JSON.stringify(questions));
+  let answers = [];
+  answers = setDefaultAnswers(questions, answers);
+  localStorage.setItem("answers", answers);
+  enableProceedButton(proceedButton);
+};
+
+
+
+
+
+const fetchQuestions = () => {
+  let questions = fetch(
     "https://my-json-server.typicode.com/Sairam-Aravind/FreshFruits/questions"
   )
+    .then(handleErrors)
     .then(response => response.json())
     .then(json => json);
-  let answers = [];
-  localStorage.setItem("questions", JSON.stringify(questions));
+  return questions;
+};
+
+
+
+
+
+
+const handleErrors = response => {
+  if (!response.ok) {
+    notifyUsers();
+    throw response.statusText;
+  }
+  return response;
+};
+
+
+
+
+
+const setDefaultAnswers = (questions, answers) => {
   for (i = 0; i < questions.length; i++) {
     switch (questions[i].type) {
       case "rating": {
@@ -44,46 +86,85 @@ const loadFirst = async () => {
       }
     }
   }
-  localStorage.setItem("answers", answers);
-  proceedButton.disabled = false;
-  proceedButton.style.backgroundColor = "#0F56B3";
-  proceedButton.style.cursor = "pointer";
+  return answers;
 };
-//module.exports = loadFirst
+//module.exports = setDefaultAnswers
 
-// this method controls the entire content that is rendered apart from the first page. it removes the contents of the old tag and replaces them with new tags based on the questions json
+
+
+
+
+
+// this method controls the entire content that is rendered apart from the first page. 
+// it removes the contents of the old tag and replaces them with new tags based on the questions json
+// a div called tab container is created according the question and the answer type. 
+//this gets rendered again on choosing a different answer
 const genrateQnA = (prevIndex, index) => {
   let question = JSON.parse(localStorage.getItem("questions"));
   if (
     parseInt(index) > question.length ||
     parseInt(prevIndex) > question.length
   ) {
-    throw "index greater than question count error";
+    console.log(pageIndexMismatchError);
+    notifyUsers();
+    throw pageIndexMismatchError;
   }
-  document.getElementById("survey-start").style.display = "none";
-  document.getElementById("survey-container").style.display = "flex";
-  document.getElementById("survey-end").style.display = "none";
+  controlDisplay("none", "flex", "none");
   let answers = localStorage.getItem("answers");
   if (answers === null || answers === undefined) {
-    throw "set answers";
+    console.log(answersmissing);
+    notifyUsers();
+    throw answersmissing;
   }
   answers = answers.split(",");
   document.getElementById("question").innerHTML = question[index].question;
-  let oldTabContainer = document.getElementById(question[prevIndex].type);
-  if (oldTabContainer !== null) {
-    oldTabContainer.parentNode.removeChild(oldTabContainer);
-  }
-  // a div called tab container is created according the question and the answer type. this gets rendered again on choosing the different answer
+  let previousQuestionType = question[prevIndex].type;
+  let questionType = question[index].type;
+  removePrevContent(previousQuestionType);
+  createTabContainerDiv(question, index, answers, questionType)
+  document.getElementById(questionType).style.display = "flex";
+  let length = question.length - 1;
+  updateNextButton(length, index);
+};
+
+
+
+
+
+const createTabContainerDiv = (question, index, answers, questionType) => {
   let tabContainerdiv = document.createElement("div");
-  tabContainerdiv.id = question[index].type;
-  let element;
-  if (question[index].type === "text") {
+  tabContainerdiv.id = questionType;
+  tabContainerdiv = genrateHTMLTags(tabContainerdiv, question, index, questionType, answers)
+  tabContainerdiv = setClassName(tabContainerdiv, questionType);
+  placeTabContainerDiv(tabContainerdiv)
+}
+
+
+
+
+
+const placeTabContainerDiv = (tabContainerdiv) => {
+  let navButton = document.getElementsByClassName("nav-button-container")[0];
+  document
+    .getElementById("survey-container")
+    .insertBefore(tabContainerdiv, navButton);
+}
+
+
+
+
+
+const genrateHTMLTags = (tabContainerdiv, question, index, questionType, answers) => {
+  if (questionType === "text") {
     let div = document.createElement("div");
-    div.className = `${question[index].type}-item`;
+    div.className = `${questionType}-item`;
     let element = createtextelement();
-    element.addEventListener("change", function() {
-      answers[index] = this.value;
-      localStorage.setItem("answers", answers);
+    if (answers[index] !== "") {
+      element.value = answers[index]
+      element.placeholder = ""
+    }
+    element.addEventListener("change", function () {
+      onPerformAction(answers, this.value, index);
     });
     div.append(element);
     tabContainerdiv.append(div);
@@ -91,54 +172,81 @@ const genrateQnA = (prevIndex, index) => {
     question[index].options &&
       question[index].options.map(option => {
         let div = document.createElement("div");
-        div.className = `${question[index].type}-item`;
-        element = document.createElement("input");
-        element.type = "radio";
-        element.id = option.points || option.value;
-        let label = document.createElement("label");
-        label.innerHTML = option.text;
-        if (question[index].type === "boolean") {
+        div.className = `${questionType}-item`;
+        if (questionType === "boolean") {
           div.style.marginBottom = "58px";
-          element.name = "boolean";
-          element.value = option.value;
-          label.value = option.value;
         }
-        if (question[index].type === "rating") {
-          element.name = "ratings";
-          element.value = option.points;
-          label.value = option.points;
-          label.addEventListener("click", function() {
-            answers[index] = this.value;
-            localStorage.setItem("answers", answers);
-            genrateQnA(index, index);
-          });
+        elements = createRadioElement(option, questionType, index, answers);
+        if (elements[2]) {
+          div.className = "selected-tab"
         }
-        if (element.id === answers[index]) {
-          element.checked = "true";
-          if (question[index].type === "rating") div.className = "selected-tab";
-        }
-        element.addEventListener("click", function() {
-          answers[index] = this.value;
-          localStorage.setItem("answers", answers);
-          genrateQnA(index, index);
-        });
+        element = elements[0];
         div.append(element);
-        div.append(label);
+        div.append(elements[1]);
         tabContainerdiv.append(div);
       });
-    tabContainerdiv = setClassName(tabContainerdiv, question[index].type);
   }
-  let navButton = document.getElementsByClassName("nav-button-container")[0];
-  document
-    .getElementById("survey-container")
-    .insertBefore(tabContainerdiv, navButton);
-  document.getElementById(question[index].type).style.display = "flex";
-  let length = question.length - 1;
-  let nextButton = document.getElementById("Next");
-  parseInt(index) === length
-    ? (nextButton.innerHTML = "Submit")
-    : (nextButton.innerHTML = "Next");
+  return tabContainerdiv
 };
+
+
+
+
+
+const createRadioElement = (option, questionType, index, answers) => {
+  let selectedTabflag = false
+  element = document.createElement("input");
+  element.type = "radio";
+  element.id = option.points || option.value;
+  let label = document.createElement("label");
+  label.innerHTML = option.text;
+  if (questionType === "boolean") {
+    element.name = "boolean";
+    element.value = option.value;
+    label.value = option.value;
+  }
+  if (questionType === "rating") {
+    element.name = "ratings";
+    element.value = option.points;
+    label.value = option.points;
+    label.addEventListener("click", function () {
+      onPerformAction(answers, this.value, index);
+      genrateQnA(index, index);
+    });
+  }
+  if (element.id === answers[index]) {
+    element.checked = "true";
+    if (questionType === "rating") {
+      selectedTabflag = true
+    };
+  }
+  element.addEventListener("click", function () {
+    onPerformAction(answers, this.value, index);
+    genrateQnA(index, index);
+  });
+  return [element, label, selectedTabflag];
+};
+//module.exports = createRadioElement
+
+
+
+
+
+const createtextelement = () => {
+  element = document.createElement("textarea");
+  element.name = "comments";
+  element.id = "text";
+  element.cols = "75";
+  element.rows = "10";
+  element.style = "resize: none;";
+  element.placeholder = "Add your answer here";
+  return element;
+};
+////module.exports = createtextelement
+
+
+
+
 
 const setClassName = (tabContainerdiv, type) => {
   switch (type) {
@@ -160,25 +268,44 @@ const setClassName = (tabContainerdiv, type) => {
   }
   return tabContainerdiv;
 };
+////module.exports = setClassName
 
-const createtextelement = () => {
-  element = document.createElement("textarea");
-  element.name = "comments";
-  element.id = "text";
-  element.cols = "75";
-  element.rows = "10";
-  element.style = "resize: none;";
-  element.placeholder = "Add your answer here";
-  return element;
-};
+
+
+
+
 // navigation buttons
 const onclickProceed = () => {
   localStorage.setItem("index", 0);
-  document.getElementById("survey-start").style.display = "none";
-  document.getElementById("survey-container").style.display = "flex";
-  document.getElementById("survey-end").style.display = "none";
+  controlDisplay("none", "flex", "none");
   genrateQnA(0, 0);
 };
+
+
+
+
+
+const disableProceedButton = proceedButton => {
+  proceedButton.disabled = true;
+  proceedButton.style.backgroundColor = "grey";
+  proceedButton.style.cursor = "not-allowed";
+};
+////module.exports = disableProceedButton
+
+
+
+
+
+const enableProceedButton = proceedButton => {
+  proceedButton.disabled = false;
+  proceedButton.style.backgroundColor = "#0F56B3";
+  proceedButton.style.cursor = "pointer";
+};
+//module.exports = enableProceedButton
+
+
+
+
 
 //Controls what happes on clicking the next button or submit button
 const onClickNext = () => {
@@ -186,43 +313,40 @@ const onClickNext = () => {
   let questions = JSON.parse(localStorage.getItem("questions"));
   if (parseInt(index) === questions.length - 1) {
     // on submit it removes all the data that is stored in the browser's local storage
-    document.getElementById("survey-start").style.display = "none";
-    document.getElementById("survey-container").style.display = "none";
-    document.getElementById("survey-end").style.display = "flex";
+    controlDisplay("none", "none", "flex");
     localStorage.setItem("index", index);
     let nextButton = document.getElementById("Next");
     if (nextButton.innerHTML === "Submit") {
-      localStorage.removeItem("index");
-      localStorage.removeItem("questions");
       let answers = localStorage.getItem("answers");
       sendAnswers(answers);
-      localStorage.removeItem("answers");
+      clearAll()
     }
   } else {
     genrateQnA(index, ++index);
     localStorage.setItem("index", index);
   }
 };
-//module.exports = onClickNext
 
-const onclickBack = () => {
-  let index = localStorage.getItem("index");
-  if (index < 1) {
-    localStorage.removeItem("index");
-    localStorage.removeItem("questions");
-    localStorage.removeItem("answers");
-    loadFirst();
-  } else {
-    genrateQnA(index, --index);
-    localStorage.setItem("index", index);
-  }
+
+
+
+
+const updateNextButton = (length, index) => {
+  let nextButton = document.getElementById("Next");
+  parseInt(index) === length
+    ? (nextButton.innerHTML = "Submit")
+    : (nextButton.innerHTML = "Next");
 };
-//module.exports = onclickBack
+//module.exports = updateNextButton
 
-const sendAnswers = answers => {
+
+
+
+const sendAnswers = (answers) => {
   console.log(answers);
   var data = new FormData();
   data.append("json", JSON.stringify(answers));
+  //alert("reload the page if you wish to take the survey again")
   fetch(
     "https://my-json-server.typicode.com/Sairam-Aravind/FreshFruits/questions",
     {
@@ -230,11 +354,80 @@ const sendAnswers = answers => {
       body: data
     }
   )
-    .then(function(res) {
+    .then(function (res) {
       return res.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       alert(JSON.stringify(data));
     });
   //sends the data for permanent storage
 };
+
+
+
+
+
+
+const onclickBack = () => {
+  let index = localStorage.getItem("index");
+  if (index < 1) {
+    clearAll()
+    loadFirst();
+  } else {
+    genrateQnA(index, --index);
+    localStorage.setItem("index", index);
+  }
+};
+
+
+
+
+
+const controlDisplay = (surveyStart, surveyContainer, surveyEnd) => {
+  document.getElementById("survey-start").style.display = surveyStart;
+  document.getElementById("survey-container").style.display = surveyContainer;
+  document.getElementById("survey-end").style.display = surveyEnd;
+};
+//module.exports = controlDisplay
+
+
+
+
+
+const notifyUsers = () => {
+  alert("Sorry for the inconvenience, Contact the support team for assistance");
+};
+//module.exports = notifyUsers
+
+
+
+
+
+const removePrevContent = type => {
+  let oldTabContainer = document.getElementById(type);
+  if (oldTabContainer !== null) {
+    oldTabContainer.parentNode.removeChild(oldTabContainer);
+  }
+};
+//module.exports = removePrevContent
+
+
+
+
+
+const onPerformAction = (answers, value, index) => {
+  answers[index] = value;
+  localStorage.setItem("answers", answers);
+};
+//module.exports = onPerformAction
+
+
+
+
+
+const clearAll = () => {
+  localStorage.removeItem("index");
+  localStorage.removeItem("questions");
+  localStorage.removeItem("answers");
+};
+//module.exports = clearAll
--- App.test.js
+++ App.test.js
@@ -1,46 +1,158 @@
 const createtextelement = require("./App");
 test("creating text element", () => {
-  expect(createtextelement().id).toBe("text");
-  expect(createtextelement().name).toBe("comments");
+  let text = createtextelement()
+  expect(text.id).toBe("text");
+  expect(text.name).toBe("comments");
 });
 
-const onclickBack = require("./App");
-jest.spyOn(window.localStorage.__proto__, "setItem");
-window.localStorage.__proto__.setItem = jest.fn();
-window.localStorage.setItem("index", 2);
-test("checking back for index value greater than zero", () => {
-  onclickBack();
-  expect(localStorage.setItem).toHaveBeenCalled();
+const controlDisplay = require("./App");
+test("choosing the approriate display value for containers with respect to index", () => {
+  let surveyStart = document.createElement("div");
+  surveyStart.setAttribute("id", "survey-start");
+  surveyStart.style.display = "block";
+  document.body.appendChild(surveyStart);
+
+  let surveyContainer = document.createElement("div");
+  surveyContainer.setAttribute("id", "survey-container");
+  surveyStart.style.display = "block";
+  document.body.append(surveyContainer);
+
+  let surveyEnd = document.createElement("div");
+  surveyEnd.setAttribute("id", "survey-end");
+  surveyStart.style.display = "block";
+  document.body.append(surveyEnd);
+
+  controlDisplay("none", "none", "flex");
+
+  let surveyend = document.getElementById("survey-end");
+
+  expect(surveyend.style.display).toBe("flex");
+  expect(surveyStart.style.display).not.toBe("block");
 });
-test("clicking back on index value zero", () => {
+
+const disableProceedButton = require("./App");
+test("disable proceed button before fetch question completes", () => {
+  let button = document.createElement("button");
+  button.setAttribute("id", "proceed-btn");
+  button.style.backgroundColor = "red";
+  document.body.append(button);
+  disableProceedButton(button);
+  let proceedButton = document.getElementById("proceed-btn");
+  expect(proceedButton).not.toBe("null");
+  expect(proceedButton.disabled).toBe(true);
+  expect(proceedButton.style.backgroundColor).toBe("grey");
+  expect(proceedButton.style.cursor).toBe("not-allowed");
+});
+
+const enableProceedButton = require("./App");
+test("enable proceed button after fetch question completes", () => {
+  let button = document.createElement("button");
+  button.setAttribute("id", "proceed-btn");
+  button.style.backgroundColor = "red";
+  button.disabled = true
+  document.body.append(button);
+  enableProceedButton(button);
+  let proceedButton = document.getElementById("proceed-btn");
+  expect(proceedButton).not.toBe("null");
+  expect(proceedButton.disabled).toBe(false);
+  expect(proceedButton.style.backgroundColor).toBe("rgb(15, 86, 179)");
+  expect(proceedButton.style.cursor).toBe("pointer");
+});
+
+const setDefaultAnswers = require("./App");
+test("setting default answer in the answers array for storing them in the local storage", () => {
+  const questions = [{
+    "type": "rating",
+    "question": "How do you rate the delivery experience?",
+    "options": [
+      {
+        "text": "Great",
+        "points": "10"
+      }]
+  }]
+  let answers = []
+  answers = setDefaultAnswers(questions, answers)
+  expect(answers["0"]).toBe(10)
+})
+
+const updateNextButton = require("./App");
+test("updating Next Button for last question", () => {
+  let button = document.createElement("button");
+  button.setAttribute("id", "Next");
+  button.innerHTML = "test"
+  document.body.append(button)
+  updateNextButton(3, 3)
+  expect(button.innerHTML).toBe("Submit")
+  updateNextButton(0, 3)
+  expect(button.innerHTML).toBe("Next")
+})
+
+const notifyUsers = require("./App");
+test("nofiying user incase of any error", () => {
+  jest.spyOn(window, "alert")
+  window.alert = jest.fn()
+  notifyUsers()
+  expect(alert).toHaveBeenCalled()
+})
+
+const onPerformAction = require("./App");
+test("for any input check if the value is stored", () => {
+  jest.spyOn(window.localStorage.__proto__, "setItem");
+  window.localStorage.__proto__.setItem = jest.fn();
+  let answers = ["10", "10", true, ""]
+  let value = "10"
+  let index = "0"
+  onPerformAction(answers, value, index)
+  expect(answers[index]).toBe("10")
+  expect(localStorage.setItem).toHaveBeenCalled()
+})
+
+const removePrevContent = require("./App");
+test("removing previous node", () => {
+  let div = document.createElement("div");
+  div.setAttribute("id", "test-rating")
+  document.body.append(div)
+  removePrevContent("test-rating")
+  expect(document.getElementById("test-rating")).toBeNull()
+  console.log(div)
+})
+
+const clearAll = require("./App")
+test("removing the data from local Storage", () => {
   jest.spyOn(window.localStorage.__proto__, "removeItem");
   window.localStorage.__proto__.removeItem = jest.fn();
-  window.localStorage.setItem("index", 1);
-  onclickBack();
-  expect(localStorage.removeItem).toHaveBeenCalled();
-});
+  clearAll()
+  expect(localStorage.removeItem).toHaveBeenCalledTimes(3)
+})
 
-const onclickNext = require("./App");
-jest.spyOn(window.localStorage.__proto__, "setItem");
-window.localStorage.__proto__.setItem = jest.fn();
-window.localStorage.setItem("index", 3);
-test("clicking next when index is zero", () => {
-  onclickNext();
-  expect(localStorage.setItem).toHaveBeenCalled();
-});
-test("clicking next on index greater than 3", () => {
-  jest.spyOn(window.localStorage.__proto__, "removeItem");
-  window.localStorage.__proto__.removeItem = jest.fn();
-  window.localStorage.setItem("index", 3);
-  onclickNext();
-  expect(localStorage.removeItem).toHaveBeenCalled();
-});
+const setClassName = require("./App");
+test("setting the class name for the div", () => {
+  let div = document.createElement("div")
+  div.setAttribute("id", "container")
+  document.body.append(div)
+  setClassName(div, "rating");
+  expect(div.className).toBe("tab-container");
+  setClassName(div, "boolean")
+  expect(div.className).toBe("radio-button-container")
+  setClassName(div, "text")
+  expect(div.className).toBe("text-area-container")
+  setClassName(div, "type")
+  expect(div.className).toBe("changed")
+})
 
-const loadFirst = require("./App");
-jest.spyOn(window.localStorage.__proto__, "setItem");
-window.localStorage.__proto__.setItem = jest.fn();
-window.localStorage.setItem("index", 3);
-test("clicking proceed", () => {
-  loadFirst();
-  expect(localStorage.setItem).toHaveBeenCalled();
-});
+const createRadioElement = require("./App");
+test("creating a radio button", () => {
+  let options = [{
+    "options": [
+      {
+        "text": "Yes, Definitely",
+        "value": false
+      }
+    ]
+  }]
+  let questionType = "boolean"
+  let index = "2"
+  let answers = ["10", "10", true, ""]
+  let elements = createRadioElement(options, questionType, index, answers)
+  expect(elements[2]).toBeFalsy()
+})
